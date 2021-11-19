a9dbdd9c0a945f56922cc8f3959512fb1294efdc
Kabsoula <amddurontex@hotmail.com>
2021-11-19 16:29:13 +0100

    updated

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import AddMovie from "../AddMovie/AddMovie";
-
+import { NavLink } from "react-router-dom";
 
 const NavBar = ({ addMovie }) => {
   return (
@@ -10,9 +10,9 @@ const NavBar = ({ addMovie }) => {
         <Container>
           <Navbar.Brand href="#home">My Movies App</Navbar.Brand>
           <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#favorite">Favorite</Nav.Link>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/features">Features</NavLink>
+          <NavLink to="favorite">Favorite</NavLink>
           </Nav>
 
           <AddMovie addMovie={addMovie} />
